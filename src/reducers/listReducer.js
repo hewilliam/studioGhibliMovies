b3cbc99d0f058294a5d2e031ebe79fefa77cae3e
William <he.william@yahoo.com>
2017-12-04 17:08:31 -0800

    added listReducer

---
+++ src/reducers/listReducer.js
@@ -0,0 +1,12 @@
+import _ from 'lodash';
+import { FETCH_MOVIES } from '../actions';
+
+
+export default function (state = {}, action) {
+    switch (action.type) {
+        case FETCH_MOVIES: 
+            _.mapKeys(action.payload.data, 'id');
+        default:
+            return state;
+    }
+}
