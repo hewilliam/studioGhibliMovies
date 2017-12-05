02dead0ac551fca7037ec9fbe8605a0d80df80f0
William <he.william@yahoo.com>
2017-12-04 22:16:48 -0800

    added console.log

--- src/reducers/listReducer.js
+++ src/reducers/listReducer.js
@@ -1,9 +1,15 @@
 import _ from 'lodash';
-import { FETCH_MOVIES } from '../actions';
+import { FETCH_MOVIES, FETCH_MOVIE } from '../actions';
 
 
 export default function (state = {}, action) {
     switch (action.type) {
+        case FETCH_MOVIE:
+            const movie = action.payload.data;
+            console.log('movie', movie)
+            const newState = {...state}
+            newState[movie.id] = movie
+            return newState;
         case FETCH_MOVIES: 
             return _.mapKeys(action.payload.data, 'id');
         default:
