323988c6eed97ddef58b530053805a4ce4da0899
William <he.william@yahoo.com>
2017-12-04 17:06:31 -0800

    added movie list action for axios

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const FETCH_MOVIES = 'fetch_movies';
+
+const ROOT_URL = 'https://ghibliapi.herokuapp.com/';
+
+export function fetchMovies() {
+    const request = axios.get(`${ROOT_URL}/films`);
+
+    return {
+        type: FETCH_MOVIES,
+        payload: request
+    }
+}
