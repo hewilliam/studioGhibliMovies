0c2267b5085cdc9b1a32e52ecc27aefbc31e7d36
William <he.william@yahoo.com>
2017-12-04 18:14:22 -0800

    added fetch movies

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,7 @@ export const FETCH_MOVIES = 'fetch_movies';
 const ROOT_URL = 'https://ghibliapi.herokuapp.com';
 
 export function fetchMovies() {
-    const request = axios.get(`${ROOT_URL}/films`);
+    const request = axios.get(`${ROOT_URL}/films`)
 
     return {
         type: FETCH_MOVIES,
