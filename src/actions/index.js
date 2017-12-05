f59ddf9a49bf92cc8cc1a4bdf07295f735040df7
William <he.william@yahoo.com>
2017-12-04 17:17:07 -0800

    fixed typo in ROOT_URL

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const FETCH_MOVIES = 'fetch_movies';
 
-const ROOT_URL = 'https://ghibliapi.herokuapp.com/';
+const ROOT_URL = 'https://ghibliapi.herokuapp.com';
 
 export function fetchMovies() {
     const request = axios.get(`${ROOT_URL}/films`);
