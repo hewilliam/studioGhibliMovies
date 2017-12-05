ac44ff0c598e3852a7694d36ad5b98c3211d87d0
William <he.william@yahoo.com>
2017-12-04 22:15:15 -0800

    added showMovie Route

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_MOVIES = 'fetch_movies';
+export const FETCH_MOVIE = 'fetch_movie';
 
 const ROOT_URL = 'https://ghibliapi.herokuapp.com';
 
@@ -11,4 +12,14 @@ export function fetchMovies() {
         type: FETCH_MOVIES,
         payload: request
     }
+}
+
+export function fetchMovie(id) {
+    const request = axios.get(`${ROOT_URL}/films/${id}`)
+
+    return {
+        type: FETCH_MOVIE,
+        payload: request
+    }
+
 }
