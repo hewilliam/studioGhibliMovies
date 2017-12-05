2e10f68d5d5293a9dc0ce6931246030880844759
William <he.william@yahoo.com>
2017-12-04 16:30:49 -0800

    added List route

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-import App from './components/app';
 import reducers from './reducers';
+import List from './components/list';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/" component={List}/>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
