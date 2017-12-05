04dc7b73eaa78d0bb3d16a247192b3703caddf1b
William <he.william@yahoo.com>
2017-12-04 18:14:45 -0800

    render a list of movies

--- src/components/list.js
+++ src/components/list.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { fetchMovies } from '../actions';
@@ -7,14 +8,31 @@ class List extends Component {
         this.props.fetchMovies();
     }
 
+    renderMovies () { //used lodash to map on a OBJECT
+        return _.map(this.props.list, list => {
+            return (
+                <li className="list-group-item" key={list.id}>
+                    {list.title}
+                </li>
+            )
+        })
+    }
 
     render () {
+        console.log(this.props.list)
         return (
             <div>
-                List
+                <h3>List of Studio Ghibli Movies</h3>
+                <ul className="list-group"> 
+                    {this.renderMovies()}
+                </ul>
             </div>
         )
     }
 }
 
-export default connect(null, {fetchMovies: fetchMovies}) (List); //same thing as mapStateToProps
+function mapStateToProps(state) {
+    return { list: state.list }
+}
+
+export default connect(mapStateToProps, {fetchMovies: fetchMovies}) (List); 
