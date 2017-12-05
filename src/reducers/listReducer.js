79f54a7d3562dcd0afd58f1b00d9da8cec1fb08e
William <he.william@yahoo.com>
2017-12-04 18:16:11 -0800

    fixed return statement in switch statement

--- src/reducers/listReducer.js
+++ src/reducers/listReducer.js
@@ -5,7 +5,7 @@ import { FETCH_MOVIES } from '../actions';
 export default function (state = {}, action) {
     switch (action.type) {
         case FETCH_MOVIES: 
-            _.mapKeys(action.payload.data, 'id');
+            return _.mapKeys(action.payload.data, 'id');
         default:
             return state;
     }
